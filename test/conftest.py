3a3cc35eed744e03d2e40cab1516954511a83304
Rory Williams <rorywilliams183@gmail.com>
2026-02-26 15:21:01 +0000

    fixed error by adding server name into mock client for testing

--- test/conftest.py
+++ test/conftest.py
@@ -10,6 +10,7 @@ from main import app
 def client():
     app.config["TESTING"] = True
     app.config["SECRET_KEY"] = "test-secret"
+    app.config["SERVER_NAME"] ="127.0.0.1:5000"
 
     with app.test_client() as client:
         with app.app_context():
