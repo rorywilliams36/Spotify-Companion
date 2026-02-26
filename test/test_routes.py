2b127b63ef4290ebc1e15eafd462ee6f1ae9d174
Rory Williams <rorywilliams183@gmail.com>
2026-02-26 14:43:46 +0000

    added routes to test playlist functions

--- test/test_routes.py
+++ test/test_routes.py
@@ -9,10 +9,26 @@ def test_login_redirects(client):
         assert response.status_code == 302
         assert "spotify-login" in response.location
 
-def test_dashboard_requires_login(client):
+def test_dashboard_route(client):
     response = client.get("/dashboard")
     assert response.status_code == 302
 
-def test_stats_route_requires_login(client):
+def test_stats_route(client):
     response = client.get("/stats/artists/long_term")
+    assert response.status_code == 302
+
+def test_playlist_route(client):
+    response = client.get("/playlist")
+    assert response.status_code == 302
+
+
+def test_create_playlist(client):
+    response = client.post("/create-playlist", 
+    data = {
+        "time_range" : 'long_term',
+        "playlist_size" : 10, 
+        "playlist_name" : "My Top Songs", 
+        "public" : False,
+        "playlist_description" : "my songs"
+    })
     assert response.status_code == 302
